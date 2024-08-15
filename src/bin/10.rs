e228603f15d25c44ad50b7e5e38792000e12cf36
mateovg <mvela092@fiu.edu>
2024-08-15 11:44:43 -0400

    statrt of 10, potential to start over tho

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,5 +1,7 @@
 advent_of_code::solution!(10);
 
+const DIRECTIONS: [(isize, isize); 4] = [(0, -1), (0, 1), (-1, 0), (1, 0)];
+
 #[derive(Debug)]
 struct Sketch {
     grid: Vec<Vec<PipeType>>,
@@ -17,9 +19,18 @@ impl Sketch {
         }
         (0, 0)
     }
+
+    fn follow_path(&self) -> Vec<(usize, usize)> {
+        let mut path = vec![];
+        let mut current = self.find_start();
+
+        
+
+        path
+    }
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum PipeType {
     NorthSouth,
     EastWest,
@@ -43,12 +54,70 @@ impl PipeType {
             _ => PipeType::None,
         }
     }
+
+    // Returns if you can enter the pipe from a given direction.
+    fn valid(&self, direction: (isize, isize)) -> bool {
+        match direction {
+            (0, -1) => match self {
+                PipeType::NorthSouth | PipeType::NorthEast | PipeType::NorthWest => true,
+                _ => false,
+            },
+            (0, 1) => match self {
+                PipeType::NorthSouth | PipeType::SouthEast | PipeType::SouthWest => true,
+                _ => false,
+            },
+            (-1, 0) => match self {
+                PipeType::EastWest | PipeType::NorthEast | PipeType::SouthEast => true,
+                _ => false,
+            },
+            (1, 0) => match self {
+                PipeType::EastWest | PipeType::NorthWest | PipeType::SouthWest => true,
+                _ => false,
+            },
+            _ => false,
+        }
+    }
+
+    fn next(&self, direction: (isize, isize)) -> Option<(isize, isize)> {
+        if !self.valid(direction) {
+            return None;
+        }
+        match direction {
+            (0, -1) => match self {
+                PipeType::NorthSouth => Some((0, -1)),
+                PipeType::NorthEast => Some((1, 0)),
+                PipeType::NorthWest => Some((-1, 0)),
+                _ => None,
+            },
+            (0, 1) => match self {
+                PipeType::NorthSouth => Some((0, 1)),
+                PipeType::SouthEast => Some((1, 0)),
+                PipeType::SouthWest => Some((-1, 0)),
+                _ => None,
+            },
+            (-1, 0) => match self {
+                PipeType::EastWest => Some((-1, 0)),
+                PipeType::NorthEast => Some((0, -1)),
+                PipeType::SouthEast => Some((0, 1)),
+                _ => None,
+            },
+            (1, 0) => match self {
+                PipeType::EastWest => Some((1, 0)),
+                PipeType::NorthWest => Some((0, -1)),
+                PipeType::SouthWest => Some((0, 1)),
+                _ => None,
+            },
+            _ => None,
+        }
+    }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let sketch = parse_input(input);
-
-    None
+    // dbg!(&sketch);
+    let path = sketch.follow_path();
+    dbg!(&path);
+    Some(path.len() as u32 / 2)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -92,5 +161,21 @@ mod tests {
     fn test_parse_input() {
         let result = parse_input(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result.grid.len(), 5);
+    }
+
+    #[test]
+    fn test_find_start() {
+        let sketch = parse_input(&advent_of_code::template::read_file("examples", DAY));
+        let result = sketch.find_start();
+        assert_eq!(result, (0, 2));
+    }
+
+    #[test]
+    fn test_valid_pipe() {
+        let pipe = PipeType::NorthSouth;
+        assert_eq!(pipe.valid((0, -1)), true);
+        assert_eq!(pipe.valid((0, 1)), true);
+        assert_eq!(pipe.valid((-1, 0)), false);
+        assert_eq!(pipe.valid((1, 0)), false);
     }
 }
