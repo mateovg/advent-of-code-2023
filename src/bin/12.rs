f1398134633862a3d69d57acf93cf5e5d5011b17
mateovg <mvela092@fiu.edu>
2024-08-16 20:12:26 -0400

    Day 12 part 1 solved

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,20 +1,49 @@
+use rayon::vec;
+
 advent_of_code::solution!(12);
 
 #[derive(Debug)]
 struct SpringSchematic {
-    groups: Vec<Vec<char>>,
+    groups: Vec<char>,
     sizes: Vec<usize>,
 }
 impl SpringSchematic {
-    fn calculate_combinations(&self, curr_p: usize, curr_n: usize) -> u32 {
+    fn combinations(&self) -> u32 {
+        let line = self.groups.clone();
+        let n = line.len();
+        let m = self.sizes.len();
+        let mut dp = &mut vec![vec![0; n + 1]; m + 1];
+        let mut next_dp = &mut vec![vec![0; n + 1]; m + 1];
 
+        dp[m][0] = 1;
+        dp[m - 1][self.sizes[m - 1]] = 1;
+
+        for pos in (0..n).rev() {
+            for group in 0..=m {
+                let max_count = if group == m { 0 } else { self.sizes[group] };
+                for count in 0..=max_count {
+                    next_dp[group][count] = 0;
+                    if matches!(line[pos], '#' | '?') {
+                        next_dp[group][count] += dp[group][count + 1];
+                    }
+                }
+                if matches!(line[pos], '.' | '?') {
+                    next_dp[group][0] += dp[group][0];
+                    if group < m {
+                        next_dp[group][max_count] += dp[group + 1][0];
+                    }
+                }
+            }
+            std::mem::swap(&mut dp, &mut next_dp);
+        }
+
+        dp[0][0]
     }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let result = parse_input(input);
-    dbg!(&result);
-    Some(0)
+    Some(result.iter().map(|x| x.combinations()).sum())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -30,6 +59,8 @@ fn parse_input(input: &str) -> Vec<SpringSchematic> {
                 .split('.')
                 .map(|g| g.chars().collect())
                 .filter(|x: &Vec<char>| !x.is_empty())
+                .map(|x| vec![x, vec!['.']].concat())
+                .flatten()
                 .collect();
             let sizes = line.split(" ").collect::<Vec<&str>>()[1]
                 .split(',')
@@ -63,7 +94,5 @@ mod tests {
     fn test_parse_input() {
         let result = parse_input(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result.len(), 6);
-        assert_eq!(result[0].groups.len(), 2);
-        assert_eq!(result[0].sizes, vec![1, 1, 3]);
     }
 }
