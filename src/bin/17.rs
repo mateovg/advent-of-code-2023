761d8db057876e09323671f79036a79dd93d8d0f
Mateo Velazquez <17498931+mateovg@users.noreply.github.com>
2024-08-29 12:14:38 -0400

    Solved 178

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,8 +1,8 @@
-use std::{isize, u32};
+use std::{cmp::Ordering, collections::BinaryHeap};
 
 advent_of_code::solution!(17);
 
-#[derive(Clone)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 enum Direction {
     Up,
     Down,
@@ -10,29 +10,50 @@ enum Direction {
     Right,
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 struct Point {
     x: i32,
     y: i32,
 }
 
-struct Crucicle {
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Crucible {
     position: Option<Point>,
     direction: Direction,
     momentum: u8,
     heat_loss: u32,
+}
+
+struct CrucibleSolver {
     grid: Vec<Vec<u32>>,
+}
+
+impl Ord for Crucible {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other
+            .heat_loss
+            .cmp(&self.heat_loss)
+            .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+impl PartialOrd for Crucible {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let crucible = parse_input(input);
-    crucible.heat_loss()
+    Some(crucible.heat_loss(0, 3))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let crucible = parse_input(input);
+    Some(crucible.heat_loss(4, 10))
 }
 
-fn parse_input(input: &str) -> Crucicle {
+fn parse_input(input: &str) -> CrucibleSolver {
     let grid = input
         .lines()
         .map(|l| {
@@ -42,13 +63,7 @@ fn parse_input(input: &str) -> Crucicle {
         })
         .collect();
 
-    Crucicle {
-        position: Some(Point { x: 0, y: 0 }),
-        direction: Direction::Right,
-        momentum: 0,
-        heat_loss: 0,
-        grid,
-    }
+    CrucibleSolver { grid }
 }
 
 impl Direction {
@@ -70,26 +85,11 @@ impl Direction {
     }
 }
 
-// impl Point {
-//     fn step(&self, direction: Direction) -> Option<Point> {
-//         let (mut x, mut y) = (self.x, self.y);
-//         match direction {
-//             Direction::Up => y -= 1,
-//             Direction::Down => y += 1,
-//             Direction::Left => x -= 1,
-//             Direction::Right => x += 1,
-//         }
-//         Point { x, y }
-//     }
-// }
-
-impl Crucicle {
-    fn step(&mut self) -> Option<Point> {
+impl Crucible {
+    fn step(&mut self) -> () {
         let direction = self.direction.clone();
-        if self.momentum == 3 {
-            return None;
-        }
-        let (mut x, mut y) = if let Some(p) = &self.position {
+
+        let (x, y) = if let Some(p) = &self.position {
             match direction {
                 Direction::Up => (p.x, p.y - 1),
                 Direction::Down => (p.x, p.y + 1),
@@ -97,21 +97,106 @@ impl Crucicle {
                 Direction::Right => (p.x + 1, p.y),
             }
         } else {
-            return None;
+            self.position = None;
+            return;
         };
-        if x < 0 || x > self.grid[0].len() as i32 || y < 0 || y > self.grid.len() as i32 {
-            return None;
-        }
-        Some(Point { x, y })
+
+        self.position = Some(Point { x, y })
     }
+}
 
-    /// For each state check, we can turn l, turn right, or continue
-    /// State = loc, dir, moves_in_dir, heat_loss
-    /// return min heatloss, return at bottom right
-    fn heat_loss(&self) -> Option<u32> {
-        let (r, c) = (self.grid.len(), self.grid[0].len());
+impl CrucibleSolver {
+    fn heat_loss(&self, min_momentum: u8, max_momentun: u8) -> u32 {
+        // BFS Search for minimum heat loss
+        let rows = self.grid.len();
+        let cols = self.grid[0].len();
+        let target = Point {
+            x: cols as i32 - 1,
+            y: rows as i32 - 1,
+        };
 
-        None
+        let mut heap = BinaryHeap::new();
+        let mut cost_map = std::collections::HashMap::new();
+
+        // Start top left, going down and right
+        heap.push(Crucible {
+            position: Some(Point { x: 0, y: 0 }),
+            direction: Direction::Down,
+            momentum: 0,
+            heat_loss: 0,
+        });
+        heap.push(Crucible {
+            position: Some(Point { x: 0, y: 0 }),
+            direction: Direction::Right,
+            momentum: 0,
+            heat_loss: 0,
+        });
+
+        // Essentially Dijkstra's
+        while let Some(crucible) = heap.pop() {
+            if let Some(p) = crucible.position {
+                // If at final and able to stop, needed for part two
+                if p.x == target.x && p.y == target.y && crucible.momentum >= min_momentum {
+                    return crucible.heat_loss;
+                }
+
+                let state_key = (p.x, p.y, crucible.direction.clone(), crucible.momentum);
+                if let Some(&new_heat_loss) = cost_map.get(&state_key) {
+                    if crucible.heat_loss > new_heat_loss {
+                        continue;
+                    }
+                }
+
+                let possible_moves = [
+                    (crucible.direction.clone(), crucible.momentum + 1),
+                    (crucible.direction.turn_left(), 1),
+                    (crucible.direction.turn_right(), 1),
+                ];
+
+                for (new_direction, new_momentum) in possible_moves.iter() {
+                    if *new_momentum > max_momentun {
+                        continue;
+                    }
+
+                    if crucible.momentum < min_momentum && *new_direction != crucible.direction {
+                        continue;
+                    }
+
+                    let mut new_crucible = Crucible {
+                        position: crucible.position,
+                        direction: new_direction.clone(),
+                        momentum: *new_momentum,
+                        heat_loss: crucible.heat_loss,
+                    };
+                    new_crucible.step();
+
+                    if let Some(new_position) = new_crucible.position {
+                        if new_position.x < 0
+                            || new_position.x >= cols as i32
+                            || new_position.y < 0
+                            || new_position.y >= rows as i32
+                        {
+                            continue;
+                        }
+
+                        new_crucible.heat_loss +=
+                            self.grid[new_position.y as usize][new_position.x as usize];
+                        let new_state = (
+                            new_position.x,
+                            new_position.y,
+                            new_direction.clone(),
+                            *new_momentum,
+                        );
+                        if new_crucible.heat_loss < *cost_map.get(&new_state).unwrap_or(&u32::MAX) {
+                            cost_map.insert(new_state, new_crucible.heat_loss);
+                            heap.push(new_crucible);
+                        }
+                    }
+                }
+            }
+        }
+
+        u32::max_value()
     }
 }
 
@@ -128,7 +213,13 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(94));
+        let input = "111111111111
+999999999991
+999999999991
+999999999991
+999999999991";
+        assert_eq!(part_two(input), Some(71));
     }
 
     #[test]
