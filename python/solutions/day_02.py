5320a1fcd1cfd7cea10f8ccc59895798daf0ea40
Mateo Velazquez <17498931+mateovg@users.noreply.github.com>
2024-09-17 13:06:37 -0400

    Day 2: Python

--- python/solutions/day_02.py
+++ python/solutions/day_02.py
@@ -10,11 +10,32 @@ RED = 12
 GREEN = 13
 BLUE = 14
 
+
 def part_one(input: str) -> int:
-    return sum(map(lambda x: x["id"], filter(lambda game: valid_game(**game), map(process, input.splitlines()))))
+    return sum(
+        map(
+            lambda x: x["id"],
+            filter(
+                valid_game,
+                map(
+                    process,
+                    input.splitlines())
+            )
+        )
+    )
+
 
 def part_two(input: str) -> int:
-    return 0
+    return sum(
+        map(
+            power_set,
+            map(
+                process,
+                input.splitlines()
+            )
+        )
+    )
+
 
 def process(line: str) -> dict:
     line_split = line.split(":")
@@ -23,24 +44,46 @@ def process(line: str) -> dict:
 
     game_dict = {
         "id": int(game_id),
-        "red": 0,
-        "green": 0,
-        "blue": 0
+        "red": set(),
+        "green": set(),
+        "blue": set(),
     }
 
     for game in games:
         for game_split in game.split(","):
             roll, color = game_split.split()
-            game_dict[color] = max(game_dict[color], int(roll))
-
-    print(game_dict)
+            game_dict[color].add(int(roll))
+    # print(game_dict)
     return game_dict
 
-def valid_game(id: int, red: int, green: int, blue: int) -> bool:
-    if red > RED: return False
-    if green > GREEN: return False
-    if blue > BLUE: return False
+
+def valid_game(game: dict) -> bool:
+    red = max(game["red"])
+    green = max(game["green"])
+    blue = max(game["blue"])
+
+    if red > RED:
+        return False
+    if green > GREEN:
+        return False
+    if blue > BLUE:
+        return False
+
     return True
+
+
+def fewest_cubes(game: dict) -> (int, int, int):
+    red = max(game["red"])
+    green = max(game["green"])
+    blue = maxt (game["blue"])
+
+    return red, green, blue
+
+
+def power_set(game: dict) -> int:
+    r, g, b = fewest_cubes(game)
+    print(f"Powerset of: {game['id']} - {r} {g} {b} - {r * g * b}")
+    return r * g * b
 
 
 def test() -> None:
@@ -48,8 +91,8 @@ def test() -> None:
     test_input_path = f"../data/examples/{day}.txt"
     test_data = get_input_data(test_input_path)
 
+    print(f"Part one: {part_one(test_data)}")
     assert part_one(test_data) == 8
-    assert part_two(test_data) == 0
+    print(f"Part two: {part_two(test_data)}")
+    assert part_two(test_data) == 2286
     print("All tests passed")
-
-
