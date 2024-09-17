274a29c26a53174de07ff5b13555fc4321f31b0d
Mateo Velazquez <17498931+mateovg@users.noreply.github.com>
2024-09-17 11:44:12 -0400

    Started python version

--- python/helpers/helpers.py
+++ python/helpers/helpers.py
@@ -8,5 +8,5 @@ def get_input_data(file_path: str) -> str:
         with open(file_path) as f:
             return f.read()
     except FileNotFoundError:
-        print("Input file for day {file_path} not found")
+        print(f"Input file for day {file_path} not found")
         sys.exit(1)
--- python/main.py
+++ python/main.py
@@ -10,14 +10,14 @@ def run_part(part_func: Callable[[str], int], data: str, part_name: str) -> None
         result = part_func(data)
         end_time = time()
         print(f"{part_name} result: {result}")
-        print(f"Run time: {end_time - start_time} seconds")
+        print(f"Run time: {(end_time - start_time) * 1000 :0.6f} ms")
     except Exception as e:
         print(f"Error running {part_name}: {e}")
 
 
 def main() -> None:
-    if len(sys.argv) != 2:
-        print("Usage: python3 main.py <day_number>")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <day_number> <test>")
         sys.exit(1)
 
     day = sys.argv[1]
@@ -25,7 +25,7 @@ def main() -> None:
     input_data = get_input_data(input_file)
 
     try:
-        day_module = importlib.import_module(f"solution.day_{day}")
+        day_module = importlib.import_module(f"solutions.day_{day}")
     except ModuleNotFoundError:
         print(f"No solution implemented for day {day}")
         sys.exit(1)
@@ -34,6 +34,8 @@ def main() -> None:
         day_module.test()
     except AttributeError:
         print(f"No tests implemented for day {day}")
+    if len(sys.argv) == 3: # Ghetto but using for now
+        return
 
     run_part(day_module.part_one, input_data, f"Day {day} Part 1")
     run_part(day_module.part_two, input_data, f"Day {day} Part 2")
